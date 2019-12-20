449d60050b5b6ae994e9b4e14ff8134d0676e0fc
R. Stange <rsta2@o2online.de>
2019-12-20 16:02:05 +0100

    Use math functions from toolchain now
    Should be faster than own implemented math functions
    and allows AArch64 build.

--- src/math.h
+++ src/math.h
@@ -2,7 +2,7 @@
 // math.h
 //
 // MiniSynth Pi - A virtual analogue synthesizer for Raspberry Pi
-// Copyright (C) 2017  R. Stange <rsta2@o2online.de>
+// Copyright (C) 2017-2019  R. Stange <rsta2@o2online.de>
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -20,16 +20,8 @@
 #ifndef _math_h
 #define _math_h
 
+#include <math.h>
+
 #define PI	3.1415926f
-
-float fabsf (float x);
-
-float sqrtf (float x);
-float expf (float x);
-float logf (float x);
-float powf (float x, float e);
-
-float sinf (float x);
-float cosf (float x);
 
 #endif
